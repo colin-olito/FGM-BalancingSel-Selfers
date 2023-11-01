f4a776ad205dc405137dfd0daa3896833c0b25ab
Colin Olito <colin.olito@gmail.com>
2023-11-01 10:58:40 +0100

    Worked on 2D FGM exploratory fig sims

--- R/functions-FGM-Simulations.R
+++ R/functions-FGM-Simulations.R
@@ -12,6 +12,7 @@
 # approximation (i.e., high-dimensional FGM)
 ###############################################
 
+# Relative probability of balancing selection ~ Mutation size
 # parameters
 # n = 50   --  no. dimensions
 # z = 1    --  wild-type displacement from optimum
@@ -59,7 +60,8 @@ relBalancingMutSize_Sims  <-  function(n = 50, z = 1, F = 1/2, h = 1/2, reps=10^
 }
 
 
-
+# Relative probability of balancing selection ~ Inbreeding Coefficient (F)
+# infinitesimal mutation size limit
 # parameters
 # n = 50   --  no. dimensions
 # z = 1    --  wild-type displacement from optimum
@@ -101,6 +103,83 @@ relBalancingSmallx_F_Sims  <-  function(n = 50, z = 1, h = 1/2, reps=10^5) {
 		PrBal.F  <-  sum((cond1 + cond2) == 2) / reps
 		# Relative probability of balancing selection
 		rBal[i]  <-  PrBal.F/PrBal
+	}
+	# Output dataframe
+	res.df  <-  data.frame("F" = F.I,
+						   "rBal" = rBal)
+	# return results
+	return(res.df)
+}
+
+
+
+
+
+
+
+# Exploration of parameter space in 2-dimensions
+# showing where we get balancing selection as we
+# vary mutation size and F.
+
+# Ancillary funcitons
+hetAdvCondSelfing(F, t) {
+	S  <-  2*F/(F + 1)
+	2*t*(1 − t − S/2) / S*(1 − 2*t)
+}
+
+# parameters
+# n = 50   --  no. dimensions
+# z = 1    --  wild-type displacement from optimum
+# h = 0.5  --  dominance
+Fisher_2D_ExploreFigSims  <-  function(z = 1, h = 1/2, reps=10^5) {
+
+	# Initial wild-type phenotype
+	A.wt  <-  c(-z, 0) 
+	# Phenotypic optimum at 0
+	Opt  <-  rep(0, 0) 
+	# Mutation sizes
+	Fisher.x    <-  c(0.05, seq(0.25, 5, by = 0.25))
+	absolute.r  <-  2*z*Fisher.x/sqrt(n)
+	r           <-  absolute.r
+	# Inbreeding values
+	F.I         <-  0:4/4
+
+	# Vector for output values
+	PosSel    <-  rep(0, times=length(F.I)*length(Fisher.x))
+	BalSel.X  <-  rep(0, times=length(F.I)*length(Fisher.x))
+	BalSel.F  <-  rep(0, times=length(F.I)*length(Fisher.x))
+	# loop over inbreeding values
+	for(i in 1:length(F.I)){
+		for(j in 1:length(absolute.r)) {
+			# random mutations
+			muts   <-  matrix(data=rnorm(n*reps), nrow=reps, ncol=n)
+			# het-/homo-zygote phenotypes
+			z.het  <-  apply(muts, MARGIN=1, function(x) {sqrt(sum((A.wt + r*h*x/sqrt(sum(x^2)) - Opt)^2))})
+			z.hom  <-  apply(muts, MARGIN=1, function(x) {sqrt(sum((A.wt + r*x/sqrt(sum(x^2)) - Opt)^2))})
+			# het-/homo-zygote selection coefficients
+			s.het  <-  exp(-0.5*z.het^2)/exp(-0.5*z^2) - 1
+			s.hom  <-  exp(-0.5*z.hom^2)/exp(-0.5*z^2) - 1
+			t.wt   <-  exp(-0.5*z^2)/exp(-0.5*z.het^2) - 1
+			t.hom  <-  exp(-0.5*z.hom^2)/exp(-0.5*z.het^2) - 1
+			# Do selection coefficients result in balancing selection?
+			# outcrossing
+			out.cond1  <-  as.numeric(s.hom < s.het)
+			out.cond2  <-  as.numeric(s.het > 0)
+			pos.cond   <-  as.numeric(s.hom > s.het)
+			PosSel.    <-  as.numeric((out.cond2 + pos.cond) == 2)
+			BalSel.X.  <-  as.numeric((out.cond1 + out.cond2) == 2)
+			PrBal      <-  sum((out.cond1 + out.cond2) == 2) / reps
+			# Inbreeding Coefficient
+			F = F.I[i]
+			# inbreeding
+			cond1    <-  as.numeric(t.tw > hetAdvCondSelfing())
+			cond2    <-  as.numeric((1 - F)*s.het > s.hom)
+			PrBal.F  <-  sum((cond1 + cond2) == 2) / reps
+			# Relative probability of balancing selection
+			rBal[i]  <-  PrBal.F/PrBal
+
+		}
+########### CONTINUE EDITING HERE ###########
 	}
 	# Output dataframe
 	res.df  <-  data.frame("F" = F.I,
